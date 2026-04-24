01ccf28c4b5ede8d35d3ae0b7546e2d2b7032248
sai sarayu <sarayu2926@gmail.com>
2026-04-24 22:23:16 +0530

    done

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://aarogyaid-insurence-1.onrender.com',
   headers: {
     'Content-Type': 'application/json',
   },
